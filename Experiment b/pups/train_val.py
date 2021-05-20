f673de03dffdd1800f33baf5fd663ff87d98a6c9
chen-chun-yu <adam@gapp.nthu.edu.tw>
2021-05-20 18:11:53 +0800

    2 new models

---
+++ Experiment b/pups/train_val.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+ori_img = cv.imread('image_original/train1.jpg')
+print(ori_img.shape)
+LR_img = cv.resize(ori_img, (int(ori_img.shape[1] / 4), int(ori_img.shape[0] / 4)))
+cv.imwrite('image_train/HR_pups_train_0001.png', ori_img)
+cv.imwrite('image_train/LR_pups_train_0001.png', LR_img)
+
+ori_img = cv.imread('image_original/train2.jpg')
+print(ori_img.shape)
+LR_img = cv.resize(ori_img, (int(ori_img.shape[1] / 4), int(ori_img.shape[0] / 4)))
+cv.imwrite('image_train/HR_pups_train_0002.png', ori_img)
+cv.imwrite('image_train/LR_pups_train_0002.png', LR_img)
+
+ori_img = cv.imread('image_original/val1.jpg')
+print(ori_img.shape)
+LR_img = cv.resize(ori_img, (int(ori_img.shape[1] / 4), int(ori_img.shape[0] / 4)))
+cv.imwrite('image_val/HR_pups_val_0001.png', ori_img)
+cv.imwrite('image_val/LR_pups_val_0001.png', LR_img)
