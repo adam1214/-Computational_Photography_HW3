92d12261067ec4fe4a54090f9bdb696a55fceee5
adam1214 <adam@gapp.nthu.edu.tw>
2021-05-20 18:40:07 +0800

    crossing & pups images

--- Experiment b/pups/train_val.py
+++ Experiment b/pups/train_val.py
@@ -14,6 +14,7 @@ cv.imwrite('image_train/LR_pups_train_0002.png', LR_img)
 
 ori_img = cv.imread('image_original/val1.jpg')
 print(ori_img.shape)
+HR_img = cv.resize(ori_img, (204, 244))
 LR_img = cv.resize(ori_img, (int(ori_img.shape[1] / 4), int(ori_img.shape[0] / 4)))
-cv.imwrite('image_val/HR_pups_val_0001.png', ori_img)
+cv.imwrite('image_val/HR_pups_val_0001.png', HR_img)
 cv.imwrite('image_val/LR_pups_val_0001.png', LR_img)
