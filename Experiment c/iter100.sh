190da38d012ec88751b0aa2e5e4465be0a75949d
adam1214 <adam@gapp.nthu.edu.tw>
2021-05-21 13:28:50 +0800

    100 model PSNR result

--- Experiment c/iter100.sh
+++ Experiment c/iter100.sh
@@ -1,4 +1,5 @@
 for (( i=1; i<=100; i++ ))
 do
-    python test.py --model_path model_trained/$model_name.pth --input_image_path image_test/LR_book.png --output_image_path result/F64B8_book_test.png --compare_image_path reference/HR_book.png --cuda
+	model_name="net_F64B8_epoch_$i"
+	python test.py --model_path ./model_trained/$model_name.pth --input_image_path ./image_test/LR_book.png --output_image_path ./result/F64B8_book_test.png --compare_image_path ./reference/HR_book.png --cuda >> log.txt
 done
